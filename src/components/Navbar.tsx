3d14393b736a66343dc8e98fd134e640a786ea42
salartalaa <salarmhmdn@gmail.com>
2025-10-30 23:41:58 +0330

    refactor(navbar): remove temporary auth condition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Image from "next/image";
-import { LogIn, LogOut, UserPlus } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { ThemeToggle } from "./theme/ThemeToggle";
 
 export default function Navbar() {
-  const user = true;
-
   return (
     <header className="max-w-7xl mx-auto mt-2 p-5 bg-background/10 shadow-md border border-border rounded-xl">
       <nav>
@@ -25,32 +23,18 @@ export default function Navbar() {
             </h3>
           </Link>
 
-          {user ? (
-            <div className="flex items-center gap-2">
-              <span className="md:text-sm text-sm">Salina</span>
-              <Avatar className="size-10">
-                <AvatarImage src="/user-placeholder.jpg" />
-                <AvatarFallback>profile</AvatarFallback>
-              </Avatar>
-              <Button className="flex items-center gap-2 dark:hover:bg-primary dark:bg-background dark:text-foreground dark:hover:text-secondary hover:bg-background hover:text-foreground">
-                <span className="md:block hidden">خروج</span>
-                <LogOut size={14} />
-              </Button>
-              <ThemeToggle />
-            </div>
-          ) : (
-            <div className="flex items-center gap-2">
-              <Button className="flex items-center gap-2 bg-background text-foreground hover:bg-accent-foreground hover:text-background">
-                <span className="md:block hidden">ورود</span>
-                <LogIn size={14} />
-              </Button>
-              <Button className="flex items-center gap-2 hover:bg-background hover:text-foreground">
-                <span className="md:block hidden">ثبت نام</span>
-                <UserPlus size={14} />
-              </Button>
-              <ThemeToggle />
-            </div>
-          )}
+          <div className="flex items-center gap-2">
+            <span className="md:text-sm text-sm">Salina</span>
+            <Avatar className="size-10">
+              <AvatarImage src="/user-placeholder.jpg" />
+              <AvatarFallback>profile</AvatarFallback>
+            </Avatar>
+            <Button className="flex items-center gap-2 hover:bg-background hover:text-foreground">
+              <span className="md:block hidden">خروج</span>
+              <LogOut size={14} />
+            </Button>
+            <ThemeToggle />
+          </div>
         </ul>
       </nav>
     </header>
